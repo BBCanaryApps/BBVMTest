d3768248bfd07c49f2b9fbc2c0a262f6adf2d489
David Lam <david@buddybuild.com>
2017-08-08 17:40:25 -0700

    Test

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-which blah
+which blah || { echo 'blah not installed, failing...'; exit 1; }
 
 # Check that pip is installed
-which pip 
+which pip || { echo 'pip not installed, failing...'; exit 1; }  
 
 # Check that system integrity protection is disabled
 if csrutil status | grep -q 'enabled'; then
-  echo "System Integrity Protection is enabled. Failing..."
+  echo "System Integrity Protection is enabled, failing..."
   exit 1
 fi
